29bc1b612f2281fcff83cdf88df4e43eb7c5cbbb
Tito Hinostroza <dev_null_1@hotmail.com>
2016-07-30 23:31:05 -0500

    Add files via upload

---
+++ Sample1_Simple/FormConfig.pas
@@ -0,0 +1,67 @@
+unit FormConfig;
+{$mode objfpc}{$H+}
+interface
+uses
+  Forms, Graphics, Buttons, StdCtrls, MiConfigXML, MisUtils;
+type
+  { TConfig }
+  TConfig = class(TForm)
+    BitCancel: TBitBtn;
+    BitAceptar: TBitBtn;
+    Edit1: TEdit;
+    Label2: TLabel;
+    procedure BitAceptarClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+  public
+    //vars to manage
+    MyText : string;
+    procedure Initiate(f: TForm);
+    procedure SaveToFile;
+  end;
+
+var
+  Config: TConfig;
+
+implementation
+{$R *.lfm}
+
+{ TConfig }
+procedure TConfig.FormCreate(Sender: TObject);
+begin
+  cfgFile.VerifyFile;
+end;
+
+procedure TConfig.Initiate(f: TForm);
+begin
+  //asociate vars to controls
+  cfgFile.Asoc_Str('MyText', @MyText, Edit1, '');
+  if not cfgFile.FileToProperties then begin
+    MsgErr(cfgFile.MsjErr);
+  end;
+end;
+
+procedure TConfig.FormShow(Sender: TObject);
+begin
+  cfgFile.PropertiesToWindow;
+end;
+
+procedure TConfig.BitAceptarClick(Sender: TObject);
+begin
+  cfgFile.WindowToProperties;
+  if cfgFile.MsjErr<>'' then begin
+    MsgErr(cfgFile.MsjErr);
+    exit;
+  end;
+  self.Close;
+end;
+
+procedure TConfig.SaveToFile;
+begin
+  if not cfgFile.PropertiesToFile then begin
+    MsgErr(cfgFile.MsjErr);
+  end;
+end;
+
+end.
+
